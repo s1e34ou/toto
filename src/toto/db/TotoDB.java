2e8ad5b90ff3e6560cd4d4fe323571f6a2856815
seojaehyoung <s1e34ou@naver.com>
2016-07-26 15:16:20 +0900

    f

--- src/toto/db/TotoDB.java
+++ src/toto/db/TotoDB.java
@@ -9,16 +9,23 @@ import java.sql.Statement;
 public class TotoDB {
 	
 
+	private TotoDB(){}
+	
+	static{
+			try{
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				System.out.println("Driver Loading Sucoess!!!");
+			}catch(ClassNotFoundException e){
+				System.out.println("Driver Loading fail");
+				System.out.println(e.getMessage());
+			} 
+	 }
+	
 	public static Connection getConnection() throws SQLException {
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-		} catch (ClassNotFoundException ex) {
-			System.out.println(ex.getMessage());
-		}
 		Connection conn = null;
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "hr";
-		String passwd = "hr";
+		String url = "jdbc:oracle:thin:@211.238.142.177:1522:orcl";
+		String user = "toto";
+		String passwd = "toto";
 
 		conn = DriverManager.getConnection(url, user, passwd);
 		return conn;
